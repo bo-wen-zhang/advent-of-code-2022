dda54cdc16cca5104af33f3a8f1be05c84274039
Bo Wen Zhang <bowenedmondzhang@gmail.com>
2022-12-03 15:10:22 +0000

    Added solution to day 3 part 2

--- day-3/main.py
+++ day-3/main.py
@@ -1,7 +1,7 @@
 import string
 
-def part_one_solution():
-    alphabet = list(string.ascii_letters)
+def part_one_solution(alphabet):
+    
     with open('input.txt') as input_file:
         lines = input_file.readlines()
         total_priority = 0
@@ -14,11 +14,28 @@ def part_one_solution():
             for item in intersection:
                 total_priority += alphabet.index(item) + 1
                 
-        print(f'Total priority is: {total_priority}')
+        print(f'Part 1: Total priority is: {total_priority}')
 
-def part_two_solution():
-    pass
+def part_two_solution(alphabet):
+    
+    with open('input.txt') as input_file:
+        lines = input_file.readlines()
+        num_of_groups = len(lines) // 3
+        total_priority = 0
+        for i in range(num_of_groups):
+            first_rucksack = set(lines[i*3].strip())
+            second_rucksack = set(lines[i*3+1].strip())
+            third_rucksack = set(lines[i*3+2].strip())
+            (badge,) = first_rucksack & second_rucksack & third_rucksack
+            total_priority += alphabet.index(badge) + 1
+        
+        print(f'Part 2: Total priority is: {total_priority}')
+
+def main():
+    alphabet = list(string.ascii_letters)
+    part_one_solution(alphabet)
+    part_two_solution(alphabet)
 
 if __name__ == '__main__':
-    part_one_solution()
-    part_two_solution()
+    main()
+    
