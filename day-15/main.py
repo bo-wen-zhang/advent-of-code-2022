3a2372882d8b2fac0a27649594a25ce967e8c9e4
Bo Wen Zhang <bowenedmondzhang@gmail.com>
2022-12-29 15:31:51 +0000

    Added solution to day 15 part 2

--- day-15/main.py
+++ day-15/main.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 def part_one_solution():
     with open('input.txt') as input_file:
@@ -23,7 +24,33 @@ def part_one_solution():
             cannot_contain_beacon.add(i)
     
     print(len(cannot_contain_beacon - confirmed_beacons))
+    
+def part_two_solution():
+    with open('input.txt') as f:
+        #horrible line of code to read but it was fun to write
+        data = list(map(lambda x: list(map(lambda z: int(z[1]),(filter(lambda y: y[0] % 2 == 1,enumerate(re.split('=|,|:', x.strip())))))), f.readlines()))
+        for d in data:
+            m_h_dist = abs(d[0]-d[2]) + abs(d[1]-d[3])
+            d.append(m_h_dist)
+        for d in data:
+            perimeter_data = []
+            p_dist = d[4]+1
+            for y in range(p_dist+1):
+                diff = p_dist - y
+                perimeter_data.append((d[0]+diff,d[1]+y))
+                perimeter_data.append((d[0]-diff,d[1]+y))
+                perimeter_data.append((d[0]+diff,d[1]-y))
+                perimeter_data.append((d[0]-diff,d[1]-y))
+            perimeter_data = [(x, y) for (x, y) in perimeter_data if (0 <=x and x <= 4000000) and (0 <= y and y <= 4000000)]
+            for (i, j) in perimeter_data:
+                for d in data:
+                    if (abs(d[0]-i) + abs(d[1]-j)) <= d[4]:
+                        break
+                else:
+                    print(i*4000000+j)
+                    return
 
 if __name__ == '__main__':
     part_one_solution()
+    part_two_solution()
     
